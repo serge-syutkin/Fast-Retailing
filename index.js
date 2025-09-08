9561a4dd0e8c8b82b1f209801fd3b9a6dbbb13c5
serge-syutkin <syutkin.s@gmail.com>
2025-09-08 16:34:13 -0600

    create news.js
    horizontal scroll

---
+++ index.js
@@ -0,0 +1,58 @@
+gsap.registerPlugin(ScrollTrigger);
+let horizontalItem = $(".news_item");
+let horizontalSection = $(".section_news");
+let moveDistance;
+function calculateScroll() {
+  // Desktop
+  let itemsInView = 1.4;
+  let scrollSpeed = 1.8;
+
+  if (window.matchMedia("(max-width: 479px)").matches) {
+    // Mobile Portrait
+    itemsInView = 1;
+    scrollSpeed = 1.2;
+  } else if (window.matchMedia("(max-width: 767px)").matches) {
+    // Mobile Landscape
+    itemsInView = 1;
+    scrollSpeed = 1.2;
+  } else if (window.matchMedia("(max-width: 991px)").matches) {
+    // Tablet
+    itemsInView = 2;
+    scrollSpeed = 1.2;
+  }
+  let moveAmount = horizontalItem.length - itemsInView;
+  let minHeight =
+    scrollSpeed * horizontalItem.outerWidth() * horizontalItem.length;
+  if (moveAmount <= 0) {
+    moveAmount = 0;
+    minHeight = 0;
+    // horizontalSection.css('height', '100vh');
+  } else {
+    horizontalSection.css("height", "200vh");
+  }
+  moveDistance = horizontalItem.outerWidth() * moveAmount;
+  horizontalSection.css("min-height", minHeight + "px");
+}
+calculateScroll();
+window.onresize = function () {
+  calculateScroll();
+};
+
+
+
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".horizontal-trigger",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,            // без времени догонки
+    // pin: true,            // опционально, если нужно фиксировать секцию
+    // markers: true         // для проверки
+  }
+});
+
+tl.to(".section_news .list", {
+  x: () => -moveDistance,
+  ease: "none"              // явно выключаем easing
+});
